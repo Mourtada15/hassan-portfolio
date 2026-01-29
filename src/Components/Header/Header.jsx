48ac3cd83b94da995af0bda2162964064ca293fd
Mourtada15 <hassanmourtada15@gmail.com>
2026-01-29 13:07:18 +0200

    edits on the existing components and adding new ones

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,8 +11,11 @@ const Header = () => {
         <p className="m-0">Hi. I'm</p>
         <h1>Hassan Mourtada,</h1>
         <p>
-          A <span style={{ color: "#55c2a0" }}>Full stack Developer</span> with
-          10 years of experience creating succeaaful websites..
+          A <span style={{ color: "#55c2a0" }}>Full stack Developer</span>{" "}
+          specializing in building modern, scalable, and high-performance web
+          applications using the MERN stack. <br />I focus on clean architecture,
+          user-centric design, and turning complex ideas into reliable digital
+          products.
         </p>
         <a
           href="/Hassan_Mourtada_Resume.pdf"
