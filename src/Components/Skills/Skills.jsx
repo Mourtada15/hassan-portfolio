d76c81f0addeabb9a8e036d6c9177393f310f95f
Mourtada15 <hassanmourtada15@gmail.com>
2026-01-31 10:00:57 +0200

    improved skills marquee

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,52 +1,26 @@
 import "./Skills.css";
 
+const skills = [
+  "HTML5", "CSS3", "JavaScript", "React", "Vue.js",
+  "Node.js", "Express.js", "RESTful APIs",
+  "MongoDB", "MySQL", "Git", "GitHub",
+  "TypeScript", "JWT Authentication",
+  "Mongoose", "Postman",
+  "Responsive Web Design",
+  "Environment Variables", "Deployment"
+];
+
 const Skills = () => {
   return (
     <section id="skills" className="skills-wrapper">
       <div className="skills-track">
-        <div className="skills-set">
-          <h4>HTML5</h4>
-          <h4>CSS3</h4>
-          <h4>JavaScript</h4>
-          <h4>React</h4>
-          <h4>Vue.js</h4>
-          <h4>Node.js</h4>
-          <h4>Express.js</h4>
-          <h4>RESTful APIs</h4>
-          <h4>MongoDB</h4>
-          <h4>MySQL</h4>
-          <h4>Git</h4>
-          <h4>GitHub</h4>
-          <h4>TypeScript</h4>
-          <h4>JWT Authentication</h4>
-          <h4>Mongoose</h4>
-          <h4>Postman</h4>
-          <h4>Responsive Web Design</h4>
-          <h4>Environment Variables</h4>
-          <h4>Deployment</h4>
-        </div>
-
-        <div className="skills-set">
-          <h4>HTML5</h4>
-          <h4>CSS3</h4>
-          <h4>JavaScript</h4>
-          <h4>React</h4>
-          <h4>Vue.js</h4>
-          <h4>Node.js</h4>
-          <h4>Express.js</h4>
-          <h4>RESTful APIs</h4>
-          <h4>MongoDB</h4>
-          <h4>MySQL</h4>
-          <h4>Git</h4>
-          <h4>GitHub</h4>
-          <h4>TypeScript</h4>
-          <h4>JWT Authentication</h4>
-          <h4>Mongoose</h4>
-          <h4>Postman</h4>
-          <h4>Responsive Web Design</h4>
-          <h4>Environment Variables</h4>
-          <h4>Deployment</h4>
-        </div>
+        {[...Array(2)].map((_, i) => (
+          <div className="skills-set" key={i}>
+            {skills.map(skill => (
+              <h4 key={`${i}-${skill}`}>{skill}</h4>
+            ))}
+          </div>
+        ))}
       </div>
     </section>
   );
